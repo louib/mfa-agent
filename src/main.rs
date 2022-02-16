c0f91529d65d905f77e3661134804f58001358a4
louib <code@louib.net>
2022-02-15 22:29:59 -0500

    First async version

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
+use std::error::Error;
+
+// use btleplug::api::{bleuuid::uuid_from_u16, Central, Manager as _, Peripheral as _, ScanFilter, WriteType};
+// use btleplug::platform::{Adapter, Manager, Peripheral};
 use gtk::prelude::*;
 use gtk::{Application, ApplicationWindow, Button};
+use tokio::time;
 
 const APP_ID: &str = "net.louib.mfa-agent";
 const APP_NAME: &str = "mfa-agent";
 const APP_TITLE: &str = "MFA Agent";
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     // Create a new application
     let app = Application::builder().application_id(APP_ID).build();
 
@@ -14,6 +20,8 @@ fn main() {
 
     // Run the application
     app.run();
+
+    Ok(())
 }
 
 pub enum Numbers {
