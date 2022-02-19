88d5f9c1658d7bcb98b8679448575d172e4c16d4
louib <code@louib.net>
2022-02-19 10:50:34 -0500

    Build is passing now

--- src/secrets_window/mod.rs
+++ src/secrets_window/mod.rs
@@ -1,8 +1,32 @@
-mod imp;
+use gtk::subclass::widget::TemplateChild;
+use gtk::{gio, glib};
+use gtk::{CompositeTemplate};
+
+mod imp {
+    use glib::subclass::object::ObjectImpl;
+    use glib::subclass::prelude::ObjectSubclass;
+
+    #[derive(Default)]
+    // #[template(file = "../ui/search.ui")]
+    pub struct SecretsWindow;
+
+    #[glib::object_subclass]
+    impl ObjectSubclass for SecretsWindow {
+        const NAME: &'static str = "SecretsWindow";
+
+        type Type = super::SecretsWindow;
+        type ParentType = glib::Object;
+    }
+
+    impl ObjectImpl for SecretsWindow {}
+}
 
 glib::wrapper! {
-    pub struct SecretsWindow(ObjectSubclass<imp::SecretsWindow>)
-        @extends gtk::ApplicationWindow, gtk::Window, gtk::Widget,
-        @implements gio::ActionGroup, gio::ActionMap, gtk::Accessible, gtk::Buildable,
-                    gtk::ConstraintTarget, gtk::Native, gtk::Root, gtk::ShortcutManager;
+    pub struct SecretsWindow(ObjectSubclass<imp::SecretsWindow>);
+}
+
+impl SecretsWindow {
+    pub fn new() -> Self {
+        glib::Object::new(&[]).unwrap()
+    }
 }
