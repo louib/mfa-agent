1ab39d597ec982121565358d8cd64153161d3d51
louib <code@louib.net>
2022-02-19 10:53:25 -0500

    Add missing extends

--- src/secrets_window/mod.rs
+++ src/secrets_window/mod.rs
@@ -1,6 +1,6 @@
 use gtk::subclass::widget::TemplateChild;
 use gtk::{gio, glib};
-use gtk::{CompositeTemplate};
+use gtk::{Application, CompositeTemplate};
 
 mod imp {
     use glib::subclass::object::ObjectImpl;
@@ -22,11 +22,15 @@ mod imp {
 }
 
 glib::wrapper! {
-    pub struct SecretsWindow(ObjectSubclass<imp::SecretsWindow>);
+    pub struct SecretsWindow(ObjectSubclass<imp::SecretsWindow>)
+        @extends gtk::ApplicationWindow, gtk::Window, gtk::Widget,
+        @implements gio::ActionGroup, gio::ActionMap, gtk::Accessible, gtk::Buildable,
+                    gtk::ConstraintTarget, gtk::Native, gtk::Root, gtk::ShortcutManager;
 }
 
 impl SecretsWindow {
-    pub fn new() -> Self {
-        glib::Object::new(&[]).unwrap()
+    pub fn new(app: &Application) -> Self {
+        // Create new window
+        glib::Object::new(&[("application", app)]).expect("Failed to create Window")
     }
 }
