64acae37a90d3a5fd2d69f7887d921780244f7b0
louib <code@louib.net>
2022-05-28 15:55:14 -0400

    Use debug version if available in dev mode

--- scripts/start_dev_proxy.sh
+++ scripts/start_dev_proxy.sh
@@ -2,4 +2,8 @@ export MFA_AGENT_IS_PROXY=true
 export MFA_AGENT_IS_DEV=true
 export MFA_AGENT_LOG_LEVEL=info
 
-mfa-agent
+if [[ -f "./target/debug/mfa-agent" ]]; then
+    ./target/debug/mfa-agent
+else
+    mfa-agent
+fi
